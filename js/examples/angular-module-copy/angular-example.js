35c571cd51ae785e4d5c5f08b4afe7452ed453ae
Aaron Sherrill <aaron@codecommando.net>
2013-05-23 15:24:44 -0400

    making it better

--- js/examples/angular-module-copy/angular-example.js
+++ js/examples/angular-module-copy/angular-example.js
@@ -10,3 +10,19 @@ define(['jquery', 'root'], function($, root){
 	});
 	
 });
+/*
+For the next example take a look at  http://jsbin.com/ibeyoh/1/edit
+	
+angular.module('MyApp1', []).controller("Goober", function($scope){
+  $scope.replace_me = "I WIN..";
+});
+angular.bootstrap($('#App1'),['MyApp1']);
+
+
+angular.module('MyApp2', []).controller("Goober", function($scope){
+  $scope.replace_me = "..at Geekery!";
+});
+angular.bootstrap($('#App2'),['MyApp2']);
+
+
+*/
